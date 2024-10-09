b1a9bb2f5f47d6c37d82504a98b397d7302cb2db
Jacqueline <jacqueline.gracey@gmail.com>
2024-10-09 09:56:19 -0700

    A button you can click

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "L03-Change the Game's Name";
+const gameName = "Devious Ducks";
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Create button:
+const button = document.createElement("button");
+// Add text and emoji to the button:
+button.innerHTML = "Quack 🦆";
+app.append(button);
+
+// Add event listener to button (when its clicked)
+button.addEventListener("click", () => {
+    // add event
+})
